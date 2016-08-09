3cf574ea19cedaead6b7c3a2f58fb9e0253b68aa
fy <fanyang_32012@outlook.com>
2016-08-09 13:53:10 +0800

    RACSubject

--- Lianxi/Lianxi/RACSubjectVC.m
+++ Lianxi/Lianxi/RACSubjectVC.m
@@ -18,8 +18,18 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
-    
+    //创建信号
+    RACSubject * subject = [RACSubject subject];
+    //订阅信号
+    [subject subscribeNext:^(id x) {
+        NSLog(@"此处先订阅信号,信号传值是%@",x);
+    }];
+    //发送信号
+    [subject sendNext:@2];
 }
+/*
+ 注意 RACSubject和RACReplaySubject的区别 RACSubject必须要先订阅信号之后才能发送信号， 而RACReplaySubject可以先发送信号后订阅. 
+ */
 
 
 
