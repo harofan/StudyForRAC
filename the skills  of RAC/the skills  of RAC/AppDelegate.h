7b2037bcd441daec6d66211e7b479e87b3bc6214
fy <fanyang_32012@outlook.com>
2016-08-24 16:36:46 +0800

    pod 文件

---
+++ the skills  of RAC/the skills  of RAC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  the skills  of RAC
+//
+//  Created by fy on 16/8/24.
+//  Copyright © 2016年 LY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
